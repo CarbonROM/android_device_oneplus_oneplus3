df605f1d435cd4db6c27b5c8599b25252327cc73
Nikhil Punathil <nikhilpe@gmail.com>
2018-08-09 16:58:29 +0100

    configpanel: Add gestures support
    * Based on the work in SuperDroidBond's device tree
    * Stripped to the bare essentials
    
    [Does not work right now]
    
    Change-Id: I5fb10d809960d000b3aaeb58602f1f1a81d3d051
    Signed-off-by: Nikhil Punathil <nikhilpe@gmail.com>

--- configpanel/src/org/carbonrom/settings/device/KeyHandler.java
+++ configpanel/src/org/carbonrom/settings/device/KeyHandler.java
@@ -20,6 +20,8 @@ package org.carbonrom.settings.device;
 import android.Manifest;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.SharedPreferences;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.hardware.Sensor;
@@ -40,13 +42,27 @@ import android.util.Log;
 import android.util.SparseIntArray;
 import android.view.KeyEvent;
 
+import android.service.notification.ZenModeConfig;
+import org.carbonrom.settings.device.ScreenOffGesture;
+
 import com.android.internal.os.DeviceKeyHandler;
 import com.android.internal.util.ArrayUtils;
+import org.carbonrom.settings.device.utils.ActionConstants;
+import org.carbonrom.settings.device.utils.Action;
 
 public class KeyHandler implements DeviceKeyHandler {
 
     private static final String TAG = KeyHandler.class.getSimpleName();
     private static final int GESTURE_REQUEST = 1;
+
+     // Supported scancodes
+    private static final int GESTURE_CIRCLE_SCANCODE = 250;
+    private static final int GESTURE_SWIPE_DOWN_SCANCODE = 251;
+    private static final int GESTURE_V_SCANCODE = 252;
+    private static final int GESTURE_LTR_SCANCODE = 253;
+    private static final int GESTURE_GTR_SCANCODE = 254;
+    private static final int GESTURE_V_UP_SCANCODE = 255;
+
     private static String FPNAV_ENABLED_PROP = "sys.fpnav.enabled";
 
     // Supported scancodes
@@ -57,6 +73,21 @@ public class KeyHandler implements DeviceKeyHandler {
     private static final int MODE_NONE = 603;
     private static final int MODE_VIBRATE = 604;
     private static final int MODE_RING = 605;
+
+    private static final int[] sSupportedGestures = new int[]{
+        GESTURE_CIRCLE_SCANCODE,
+        GESTURE_SWIPE_DOWN_SCANCODE,
+        GESTURE_V_SCANCODE,
+        GESTURE_V_UP_SCANCODE,
+        GESTURE_LTR_SCANCODE,
+        GESTURE_GTR_SCANCODE,
+        MODE_TOTAL_SILENCE,
+        MODE_ALARMS_ONLY,
+        MODE_PRIORITY_ONLY,
+        MODE_NONE,
+        MODE_VIBRATE,
+        MODE_RING
+    };
 
     private static final int GESTURE_WAKELOCK_DURATION = 3000;
 
@@ -75,6 +106,7 @@ public class KeyHandler implements DeviceKeyHandler {
     private final PowerManager mPowerManager;
     private final NotificationManager mNotificationManager;
     private final AudioManager mAudioManager;
+    private Context mGestureContext = null;
     private EventHandler mEventHandler;
     private SensorManager mSensorManager;
     private Sensor mProximitySensor;
@@ -86,28 +118,117 @@ public class KeyHandler implements DeviceKeyHandler {
 
     public KeyHandler(Context context) {
         mContext = context;
+        mEventHandler = new EventHandler();
         mPowerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         mNotificationManager
                 = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        mEventHandler = new EventHandler();
-        mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-        mGestureWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
-                "GestureWakeLock");
-
-        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        mProximitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mProximityWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                "ProximityWakeLock");
+         mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
         if (mVibrator == null || !mVibrator.hasVibrator()) {
             mVibrator = null;
+        }
+         try {
+            mGestureContext = mContext.createPackageContext(
+                    "org.carbonrom.settings.device", Context.CONTEXT_IGNORE_SECURITY);
+        } catch (NameNotFoundException e) {
         }
     }
 
     private class EventHandler extends Handler {
         @Override
         public void handleMessage(Message msg) {
-            if (msg.arg1 == FLIP_CAMERA_SCANCODE) {
-                mGestureWakeLock.acquire(GESTURE_WAKELOCK_DURATION);
-                doHapticFeedback();
+            KeyEvent event = (KeyEvent) msg.obj;
+            String action = null;
+            switch(event.getScanCode()) {
+            case GESTURE_CIRCLE_SCANCODE:
+                action = getGestureSharedPreferences()
+                        .getString(ScreenOffGesture.PREF_GESTURE_CIRCLE,
+                        ActionConstants.ACTION_CAMERA);
+                        doHapticFeedback();
+                break;
+            case GESTURE_SWIPE_DOWN_SCANCODE:
+                action = getGestureSharedPreferences()
+                        .getString(ScreenOffGesture.PREF_GESTURE_DOUBLE_SWIPE,
+                        ActionConstants.ACTION_MEDIA_PLAY_PAUSE);
+                        doHapticFeedback();
+                break;
+            case GESTURE_V_SCANCODE:
+                action = getGestureSharedPreferences()
+                        .getString(ScreenOffGesture.PREF_GESTURE_ARROW_DOWN,
+                        ActionConstants.ACTION_VIB_SILENT);
+                        doHapticFeedback();
+                break;
+            case GESTURE_V_UP_SCANCODE:
+                action = getGestureSharedPreferences()
+                        .getString(ScreenOffGesture.PREF_GESTURE_ARROW_UP,
+                        ActionConstants.ACTION_TORCH);
+                        doHapticFeedback();
+                break;
+            case GESTURE_LTR_SCANCODE:
+                action = getGestureSharedPreferences()
+                        .getString(ScreenOffGesture.PREF_GESTURE_ARROW_LEFT,
+                        ActionConstants.ACTION_MEDIA_PREVIOUS);
+                        doHapticFeedback();
+                break;
+            case GESTURE_GTR_SCANCODE:
+                action = getGestureSharedPreferences()
+                        .getString(ScreenOffGesture.PREF_GESTURE_ARROW_RIGHT,
+                        ActionConstants.ACTION_MEDIA_NEXT);
+                        doHapticFeedback();
+                break;
+            case MODE_TOTAL_SILENCE:
+                setZenMode(Settings.Global.ZEN_MODE_NO_INTERRUPTIONS);
+                break;
+            case MODE_ALARMS_ONLY:
+                setZenMode(Settings.Global.ZEN_MODE_ALARMS);
+                break;
+            case MODE_PRIORITY_ONLY:
+                setZenMode(Settings.Global.ZEN_MODE_IMPORTANT_INTERRUPTIONS);
+                setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+                break;
+            case MODE_NONE:
+                setZenMode(Settings.Global.ZEN_MODE_OFF);
+                setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+                break;
+            case MODE_VIBRATE:
+                setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE);
+                break;
+            case MODE_RING:
+                setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+                break;
             }
+             if (action == null || action != null && action.equals(ActionConstants.ACTION_NULL)) {
+                return;
+            }
+            if (action.equals(ActionConstants.ACTION_CAMERA)
+                    || !action.startsWith("**")) {
+                Action.processAction(mContext, ActionConstants.ACTION_WAKE_DEVICE, false);
+            }
+            Action.processAction(mContext, action, false);
         }
+    }
+
+	private void setZenMode(int mode) {
+        mNotificationManager.setZenMode(mode, null, TAG);
+        if (mVibrator != null) {
+            mVibrator.vibrate(50);
+        }
+    }
+     private void setRingerModeInternal(int mode) {
+        mAudioManager.setRingerModeInternal(mode);
+        if (mVibrator != null) {
+            mVibrator.vibrate(50);
+        }
+    }
+
+    private SharedPreferences getGestureSharedPreferences() {
+        return mGestureContext.getSharedPreferences(
+                ScreenOffGesture.GESTURE_SETTINGS,
+                Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS);
     }
 
     private boolean hasSetupCompleted() {
